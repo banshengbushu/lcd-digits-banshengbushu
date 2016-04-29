67b750cb028af7807488888edb406e2603fee77c
banshenbushu <wangdanna1995@outlook.com>
2016-04-29 17:21:53 +0800

    second commit

--- main/main.js
+++ main/main.js
@@ -1,23 +1,23 @@
 function printLCDDigits(input) {
+    var LCDTotalDigits = loadLCDTotalDigits();
     var LCDArray = buildLCDArray(input);
-    var expectPrint = showLCDDigits(LCDArray);
+    var expectPrint = showLCDDigits(LCDArray, LCDTotalDigits);
     console.log(expectPrint);
 }
 
 function buildLCDArray(input) {
 
-    return input.toString().split('');
+    return parseInt(input.toString().split(''));
 }
 
-function showLCDDigits(LCDArray) {
-    var LCDTotalDigits = loadLCDTotalDigits();
-
+function showLCDDigits(LCDArray,LCDTotalDigits) {
+  
     for (var i = 0; i < LCDArray.length; i++) {
-        var expectDigits = "";
-
-        expectDigits += LCDTotalDigits[LCDArray[i]][0] + '\n' +
-                        LCDTotalDigits[LCDArray[i]][1] + '\n' +
-                        LCDTotalDigits[LCDArray[i]][2];
-    }
+		var expectDigits = [];
+			expectDigits[i] = LCDTotalDigits[i][LCDArray[0]]+ ' '+
+							  LCDTotalDigits[i][LCDArray[1]] + ' ' +
+						      LCDTotalDigits[i][LCDArray[2]];
+		}
+	
     return expectDigits;
 }
--- test/main-test.js
+++ test/main-test.js
@@ -1,6 +1,4 @@
 describe('lcd-digits', function () {
-
-    describe('integration testing', function () {
     var input;
     var LCDTotalDigits;
 
@@ -9,18 +7,37 @@ describe('lcd-digits', function () {
         LCDTotalDigits = loadLCDTotalDigits();
     });
 
+    describle('buildLCDArray', function () {
+
+        it('should print correct text', function () {
+            var LCDArray = [6, 1, 3];
+            expect(buildLCDArray(input)).toEqual(LCDArray);
+        });
+    });
+
+    describle('showLCDDigits', function () {
+        it('should print correct text', function () {
+            var expectDigits = ['._. ... ._.',
+                                '|_. ..| ._|',
+                                '|_| ..| ._|'];
+            expect(showLC0DDigits(LCDArray, LCDTotalDigits)).toEqual(expectDigits);
+        });
+
+    });
+
+    describle('should print correct text', function () {
+
         it('should print correct text', function () {
             spyOn(console, 'log');
 
             printLCDDigits(input);
 
-            var expectDigits =
+            var print =
                 '._.' + '...' + '._.' + '\n' +
                 '|_.' + '..|' + '._|' + '\n' +
                 '|_|' + '..|' + '._|';
 
-            expect(console.log).toHavaBeenCalledWith(expectDigits);
+            expect(console.log).toHavaBeenCalledWith(print);
         });
-
     });
 });
