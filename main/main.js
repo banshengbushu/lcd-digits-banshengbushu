d9e5e291a392432128d419c6c2ac44c801882055
banshenbushu <wangdanna1995@outlook.com>
2016-05-02 10:50:29 +0800

    modified

--- main/main.js
+++ main/main.js
@@ -1,8 +1,8 @@
 function printLCDDigits(input) {
     var LCDTotalDigits = loadLCDTotalDigits();
     var LCDArray = buildLCDArray(input);
-    var expectPrint = showLCDDigits(LCDArray, LCDTotalDigits);
-    console.log(expectPrint);
+    var expectPrint = generateLCDDigits(LCDArray, LCDTotalDigits);
+    print(expectPrint);
 }
 
 function buildLCDArray(input) {
@@ -10,15 +10,36 @@ function buildLCDArray(input) {
     return input.toString().split('');
 }
 
-function showLCDDigits(LCDArray, LCDTotalDigits) {
-    var expectDigits = '';
+function generateLCDDigits(LCDArray, LCDTotalDigits) {
+    var expectLCDDigits = [];
     for (var i = 0; i < LCDArray.length; i++) {
+        var LCDDigits = findSameDigits(LCDArray[i], LCDTotalDigits);
+        expectLCDDigits.push(LCDDigits);
+    }
+    return expectLCDDigits;
+}
 
-        expectDigits += '\n' + LCDTotalDigits[i][LCDArray[0]] +
-                               LCDTotalDigits[i][LCDArray[1]] +
-                               LCDTotalDigits[i][LCDArray[2]];
+function findSameDigits(LCDArray, LCDTotalDigits) {
+    var sameDigits;
 
+    for (var j = 0; j < LCDTotalDigits.length; j++) {
+        if (LCDTotalDigits[j].key === LCDArray) {
+            sameDigits = LCDTotalDigits[j];
+        }
     }
 
-    return expectDigits;
+    return sameDigits;
+}
+
+function print(expectDigits) {
+    var firstRow = '\n';
+    var secondRow = '\n';
+    var thirdRow = '\n';
+    for (var i = 0; i < expectDigits.length; i++) {
+        firstRow += expectDigits[i].value[0] + ' ';
+        secondRow += expectDigits[i].value[1] + ' ';
+        thirdRow += expectDigits[i].value[2] + ' ';
+    }
+
+    console.log(firstRow + secondRow + thirdRow);
 }
